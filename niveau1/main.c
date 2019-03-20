cff304c48d345961046b7882f3316126361da0dd
Quentin Bastian <bastian24u@fst-ats-311-l08.fst.site.univ-lorraine.fr>
2019-03-20 16:26:33 +0100

    Niveau 1

---
+++ niveau1/main.c
@@ -0,0 +1,174 @@
+/**
+ * \file main.c
+ * \brief Programme principal du niveau 0, à compléter
+ * \author Mathieu Constant
+ * \version 0.1
+ * \date 13 mars 2019
+ */
+
+#include "sdl-light.h"
+
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 1288
+
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 725
+
+/**
+ * \brief Taille d'une boule
+ */
+#define BALL_SIZE 42
+
+
+
+
+
+
+/**
+ * \brief Représentation du monde du jeu
+ */
+
+struct world_s{
+    int gameover;
+    
+    };
+
+typedef struct world_s world_t;
+
+
+
+/**
+ * \brief La fonction initialise les données du monde du jeu
+ * \param world les données du monde
+ */
+
+
+void init_data(world_t * world){
+
+    world->gameover = 0;
+    
+}
+
+
+/**
+ * \brief La fonction nettoie les données du monde
+ * \param world les données du monde
+ */
+
+
+void clean_data(world_t *world){
+    
+}
+
+
+
+
+/**
+ * \brief La fonction indique si le jeu est fini en fonction des données du monde
+ * \param world les données du monde
+ * \return 1 si le jeu est fini, 0 sinon
+ */
+
+int is_game_over(world_t *world){
+    return world->gameover;
+}
+
+
+/**
+ * \brief La fonction met à jour les données en tenant compte de la physique du monde
+ * \param les données du monde
+ */
+
+void update_data(world_t *world){
+}
+
+
+/**
+ * \brief La fonction initialise les transparences des différentes surfaces
+ * \param screen la surface correspondant à l'écran de jeu
+ * \param world les données du monde
+ */
+
+void  init_graphics(SDL_Surface *screen, world_t *world){
+    
+}
+
+
+
+
+
+
+
+
+/**
+ * \brief La fonction rafrachit l'écran en fonction de l'état des données du monde
+ * \param screen la surface de l'écran de jeu
+ * \param world les données du monde
+ */
+
+void refresh_graphics(SDL_Surface *screen, world_t *world){
+    
+
+    
+    
+    refresh_surface(screen);
+}
+
+
+
+/**
+ * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+ * \param event paramètre qui contient les événements
+ * \param world les données du monde
+ */
+
+
+void handle_events(SDL_Event *event,world_t *world){
+    
+    while( SDL_PollEvent( event ) ) {
+        //Si l'utilisateur a cliqué sur le X de la fenêtre
+        if( event->type == SDL_QUIT ) {
+            //On quitte le programme
+            world->gameover = 1;
+        }
+    
+        
+        
+    }
+    
+}
+
+/**
+ *  \brief programme principal qui implémente la boucle du jeu
+ */
+
+
+int main( int argc, char* args[] )
+{
+    SDL_Event event;
+    world_t world;
+    SDL_Surface *screen;
+
+    screen = init_sdl(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
+    init_data(&world);
+    init_graphics(screen,&world);
+    
+    
+    while(!is_game_over(&world)){
+        handle_events(&event,&world);
+        update_data(&world);
+        refresh_graphics(screen,&world);
+        SDL_Delay(10);
+    }
+    
+    clean_data(&world);
+    quit_sdl();
+    
+    
+    return 0;
+}
